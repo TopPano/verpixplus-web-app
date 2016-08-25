e81667777504c1111768d65d0a8c8f51eddae1e8
SuJiaKuan <jiakuan.su@toppano.in>
2016-08-25 11:30:32 +0800

    Add components to landing page

---
+++ shared/components/Pages/Landing/ServiceBlock/Service.jsx
@@ -0,0 +1,39 @@
+'use strict';
+
+import React, { Component, PropTypes } from 'react';
+
+if (process.env.BROWSER) {
+  require('./Service.css');
+}
+
+const propTypes = {
+  icon: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired
+};
+
+const defaultProps = {
+};
+
+export default class Service extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { icon, name, desc } = this.props;
+
+    return (
+      <div className="service-component col-md-4 content-boxes-v6 md-margin-bottom-50">
+        <i className={`rounded-x icon-${icon}`}></i>
+        <h1 className="title-v3-md text-uppercase margin-bottom-10">{name}</h1>
+        <p>{desc}</p>
+      </div>
+    );
+  }
+}
+
+Service.propTypes = propTypes;
+Service.defaultProps = defaultProps;
+
+export default Service;
--- shared/components/Pages/Landing/index.jsx
+++ shared/components/Pages/Landing/index.jsx
@@ -1,20 +1,35 @@
 'use strict';
 
 import React, { Component } from 'react';
+
+import Intro from './Intro';
+import ServiceBlock from './ServiceBlock';
+
 if (process.env.BROWSER) {
   require('./Landing.css');
 }
 
-export default class Landing extends Component {
+const propTypes = {
+};
+
+const defaultProps = {
+};
+
+class Landing extends Component {
   constructor(props) {
     super(props);
   }
   render() {
     return (
-      <div className='landing-component'>
+      <div className="landing-component">
+        <Intro />
+        <ServiceBlock />
       </div>
     );
   }
 }
 
-Landing.displayName = 'Landing';
+Landing.propTypes = propTypes;
+Landing.defaultProps = defaultProps;
+
+export default Landing;
