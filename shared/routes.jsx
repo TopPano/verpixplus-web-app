39bf22cd3e7d5620b730e0f41d99285d045fdc1e
SuJiaKuan <jiakuan.su@toppano.in>
2016-08-31 16:49:14 +0800

    Remove unnecessary routing

--- shared/routes.jsx
+++ shared/routes.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, Redirect } from 'react-router';
 
 import App from './containers/App';
 import MainLayout from './containers/layouts/MainLayout';
 
 import HomePageContainer from './containers/pages/Home';
-import ExplorerPageContainer from './containers/pages/Explorer';
-import ViewerPageContainer from './containers/pages/Viewer';
-import PersonalPageContainer from './containers/pages/Personal';
 import SignInPageContainer from './containers/pages/SignIn';
 import FAQPageComponent from './components/Pages/FAQ';
 
@@ -15,12 +12,9 @@ export default (
   <Route component={App}>
     <Route component={MainLayout}>
       <Route component={HomePageContainer} path='/' />
-      <Route component={SignInPageContainer} path='/signin' />
-      <Route component={ViewerPageContainer} path='/viewer/@:postId' />
-      <Route component={ExplorerPageContainer} path='/explore' />
       <Route component={FAQPageComponent} path='/faq' />
-      <Route component={ViewerPageContainer} path='/viewer/@:postId' />
-      <Route component={PersonalPageContainer} path='/@:id' />
+      <Route component={SignInPageContainer} path='/signin' />
+      <Redirect from='*' to='/' />
     </Route>
   </Route>
 );
