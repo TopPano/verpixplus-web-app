2179a0026c092411d5790fd1fc78d4681583c951
SuJiaKuan <jiakuan.su@toppano.in>
2016-10-03 17:07:58 +0800

    Enable resize FrameCarouselWindow component

--- shared/components/Pages/Editor/FramePanel/FrameCarouselWindow.jsx
+++ shared/components/Pages/Editor/FramePanel/FrameCarouselWindow.jsx
@@ -4,6 +4,8 @@ import React, { Component, PropTypes } from 'react';
 import classNames from 'classnames';
 import Rnd from 'react-rnd';
 
+import { FRAMES_LIMIT } from 'constants/editor';
+
 if (process.env.BROWSER) {
   require('./FrameCarouselWindow.css');
 }
@@ -28,43 +30,100 @@ class FrameCarouselWindow extends Component {
     super(props);
 
     // Bind "this" to member functions
-    this.handleDragStart = this.handleDragStart.bind(this);
-    this.handleDragStop = this.handleDragStop.bind(this);
+    this.handleResizeStart = this.handleResizeStart.bind(this);
+    this.handleResizeStop = this.handleResizeStop.bind(this);
+    this.handleChangeStart = this.handleChangeStart.bind(this);
+    this.handleChangeStop = this.handleChangeStop.bind(this);
     this.handleDrag = this.handleDrag.bind(this);
+    this.handleWindowMouseMove = this.handleWindowMouseMove.bind(this);
 
     // Initialize state
     this.state = {
       isInit: false,
-      isDragged: false
+      isChanging: false,
+      resize: {
+        isResizing: false
+      }
     };
   }
 
   componentDidMount() {
     const {
       framesNum,
+      unitWidth,
+      height,
       lower,
-      unitWidth
+      upper
     } = this.props;
 
     this.updatePosition(this.calculatePosition(lower, framesNum, unitWidth));
+    this.updateSize(this.calculateSize(lower, upper, unitWidth, height));
+
+    if (process.env.BROWSER) {
+      window.addEventListener('mousemove', this.handleWindowMouseMove, true);
+    }
+
     this.setState({
       isInit: true
     });
   }
 
-  // Handler for start dragging
-  handleDragStart() {
+  componentDidUpdate() {
+    const {
+      framesNum,
+      unitWidth,
+      height,
+      lower,
+      upper
+    } = this.props;
+
+    this.updatePosition(this.calculatePosition(lower, framesNum, unitWidth));
+    this.updateSize(this.calculateSize(lower, upper, unitWidth, height));
+  }
+
+  componentWillUnmount() {
+    if (process.env.BROWSER) {
+      window.removeEventListener('mousemove', this.handleWindowMouseMove);
+    }
+  }
+
+  // Handler for resize start
+  handleResizeStart(direction, styleSize, clientSize, e) {
+    this.handleChangeStart();
+
     this.setState({
-      isDragged: true
+      resize: {
+        isResizing: true,
+        direction,
+        lastX: e.clientX
+      }
+    });
+  }
+
+  // Handler for resize stop
+  handleResizeStop() {
+    this.handleChangeStop();
+
+    this.setState({
+      resize: {
+        isResizing: false
+      }
+    });
+  }
+
+  // Handler for start changing size or position
+  handleChangeStart() {
+    this.setState({
+      isChanging: true
     });
 
     this.props.playerPause();
   }
 
-  // Handler for stop dragging
-  handleDragStop() {
+  // Handler for start changing size or position
+  handleChangeStop() {
     this.setState({
-      isDragged: false
+      isChanging: false
     });
 
     this.props.playerPlay();
@@ -80,10 +139,73 @@ class FrameCarouselWindow extends Component {
     } = this.props;
     const offset = parseInt(ui.deltaX / unitWidth, 10);
 
-    trim({
-      lower: lower + offset,
-      upper: upper + offset
-    });
+    if (offset !== 0) {
+      trim({
+        lower: lower + offset,
+        upper: upper + offset
+      });
+    }
+  }
+
+  // Handler for mouse moving on window
+  // XXX:
+  // The onResize handler of Rnd cannot work on our case because the Swiper (Carousel) "absorbs" the mousemove event.
+  // To solve the bug, we add our own event listener for mousemove event,
+  // by calling window.addEventListener with "useCapture" option be true.
+  handleWindowMouseMove(e) {
+    const {
+      unitWidth,
+      framesNum,
+      lower,
+      upper,
+      trim
+    } = this.props;
+    const {
+      isResizing,
+      direction,
+      lastX
+    } = this.state.resize;
+
+    if (isResizing) {
+      const offset = parseInt((e.clientX - lastX) / unitWidth, 10);
+
+      if (Math.abs(offset) >= 1) {
+        let newUpper = upper;
+        let newLower = lower;
+        let newOffset = offset;
+
+        if (direction === 'right') {
+          newUpper =
+            (((upper + offset) >= (framesNum) && (framesNum - lower) < FRAMES_LIMIT)) ? (framesNum) :
+            ((upper + offset) >= (lower + FRAMES_LIMIT)) ? (lower + FRAMES_LIMIT) :
+            ((upper + offset) <= (lower + 1)) ? (lower + 1) :
+            (upper + offset);
+          newOffset = newUpper - upper;
+        } else {
+          newLower =
+            (((lower + offset) <= 0) && (upper < FRAMES_LIMIT)) ? 0 :
+            ((lower + offset) <= (upper - FRAMES_LIMIT)) ? (upper - FRAMES_LIMIT) :
+            ((lower + offset) >= (upper - 1)) ? (upper - 1) :
+            (lower + offset);
+          newOffset = newLower - lower;
+        }
+
+        if (newOffset !== 0) {
+          this.setState({
+            resize: {
+              isResizing: true,
+              direction,
+              lastX: lastX + (unitWidth * newOffset)
+            }
+          });
+
+          trim({
+            lower: newLower,
+            upper: newUpper
+          });
+        }
+      }
+    }
   }
 
   // Calculate window width
@@ -91,11 +213,21 @@ class FrameCarouselWindow extends Component {
     return (upper - lower) * unitWidth;
   }
 
+  // Calculate window size
+  calculateSize(lower, upper, unitWidth, height) {
+    return {
+      width: this.calculateWidth(lower, upper, unitWidth),
+      height
+    };
+  }
+
   // Calculate window left and right bounds for draggin
   calculateBounds(lower, upper, framesNum, unitWidth) {
     return {
-      left: this.calculatePosition(0, framesNum, unitWidth).x,
-      right: -(this.calculateWidth(lower, upper, unitWidth))
+      top: 0,
+      right: -(this.calculateWidth(lower, upper, unitWidth)),
+      bottom: 0,
+      left: this.calculatePosition(0, framesNum, unitWidth).x
     };
   }
 
@@ -112,6 +244,11 @@ class FrameCarouselWindow extends Component {
     this.refs.rnd.updatePosition(position);
   }
 
+  // Update the size of window
+  updateSize(size) {
+    this.refs.rnd.updateSize(size);
+  }
+
   render() {
     const {
       framesNum,
@@ -122,12 +259,12 @@ class FrameCarouselWindow extends Component {
     } = this.props;
     const {
       isInit,
-      isDragged
+      isChanging
     } = this.state;
     const componentClass = classNames({
       'frame-carousel-window-component': true,
       'active': isInit,
-      'dragged': isDragged
+      'changing': isChanging
     });
     const componentProps = {
       ref: 'rnd',
@@ -140,10 +277,27 @@ class FrameCarouselWindow extends Component {
       },
       moveAxis: 'x',
       moveGrid: [unitWidth, unitWidth],
+      resizeGrid: [unitWidth, unitWidth],
       bounds: this.calculateBounds(lower, upper, framesNum, unitWidth),
-      onDragStart: this.handleDragStart,
+      minHeight: height,
+      minWidth: this.calculateWidth(0, 1, unitWidth),
+      maxWidth: this.calculateWidth(0, FRAMES_LIMIT, unitWidth),
+      maxHeight: height,
+      isResizable: {
+        top: false,
+        right: true,
+        bottom: false,
+        left: true,
+        topRight: false,
+        bottomRight: false,
+        bottomLeft: false,
+        topLeft: false
+      },
+      onDragStart: this.handleChangeStart,
       onDrag: this.handleDrag,
-      onDragStop: this.handleDragStop
+      onDragStop: this.handleChangeStop,
+      onResizeStart: this.handleResizeStart,
+      onResizeStop: this.handleResizeStop
     };
 
     return (
