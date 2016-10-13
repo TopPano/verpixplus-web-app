fbfd02758a1f9ac00ff201377dfd1f7c112d7cfc
SuJiaKuan <jiakuan.su@toppano.in>
2016-10-13 18:54:41 +0800

    Use async queue in applyImagesFilters.js

--- shared/actions/editor/applyImagesFilters.js
+++ shared/actions/editor/applyImagesFilters.js
@@ -1,3 +1,4 @@
+import async from 'async';
 import randomstring from 'randomstring';
 
 import {
@@ -5,39 +6,58 @@ import {
   Promise
 } from 'lib/utils';
 
+const NUM_CONCURRENT_TASKS = 3;
+
+function applyImageFiltersAsync(img, dimension, filters, callback) {
+  const canvas = document.createElement('CANVAS');
+  const id = randomstring.generate({
+    length: 20,
+    charset: 'alphabetic'
+  });
+
+  canvas.setAttribute('id', id);
+  canvas.width = dimension.width;
+  canvas.height = dimension.height;
+  canvas.style.display = 'none';
+  canvas.style.visibility = 'hidden';
+  document.body.appendChild(canvas);
+
+  Caman(`#${id}`, function () {
+    applyImageFilters(this, img, dimension, filters).then((appliedImgData) => {
+      const dataUrl = canvas.toDataURL('image/jpeg');
+      document.body.removeChild(canvas);
+      callback(null, {
+        appliedImgData,
+        dataUrl
+      })
+    });
+  });
+}
+
 export default function applyImagesFilters(imgs, dimension, filters) {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     const appliedData = new Array(imgs.length);
     const dataUrls = new Array(imgs.length);
-    let counter = 0;
+    const queue = async.queue((task, callback) => {
+      applyImageFiltersAsync(task.img, dimension, filters, callback);
+    }, NUM_CONCURRENT_TASKS);
+
+    queue.drain = () => {
+      resolve({
+        appliedData,
+        dataUrls
+      });
+    }
 
     imgs.forEach((img, idx) => {
-      const canvas = document.createElement('CANVAS');
-      const id = randomstring.generate({
-        length: 20,
-        charset: 'alphabetic'
-      });
-
-      canvas.setAttribute('id', id);
-      canvas.width = dimension.width;
-      canvas.height = dimension.height;
-      canvas.style.display = 'none';
-      canvas.style.visibility = 'hidden';
-      document.body.appendChild(canvas);
-
-      Caman(`#${id}`, function () {
-        applyImageFilters(this, img, dimension, filters).then((appliedImgData) => {
-          appliedData[idx] = appliedImgData;
-          dataUrls[idx] = canvas.toDataURL('image/jpeg');
-          counter++;
-          document.body.removeChild(canvas);
-          if (counter === imgs.length) {
-            resolve({
-              appliedData,
-              dataUrls
-            });
-          }
-        });
+      queue.push({ img }, (err, result) => {
+        if(err) {
+          reject(err);
+        } else {
+          console.log(idx);
+          appliedData[idx] = result.appliedImgData;
+          dataUrls[idx] = result.dataUrl;
+        }
       });
     });
   });
